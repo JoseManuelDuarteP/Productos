50e467af122ffd80f854d398eab8ebe706fdf65c
Jose Manuel Duarte Peris <jose.duarte@cuatroochenta.com>
2025-05-22 17:33:07 +0200

    feat: añadidas excepciones

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -14,11 +14,11 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         String marca;
-        float precio;
+        float precio = 0;
         float descuento;
         String tipoCereal;
         String tipoVino;
-        int gradoAlcohol;
+        float gradoAlcohol;
 
         int option;
         do {
@@ -29,48 +29,48 @@ public class Main {
             System.out.println("4. Listar todos los productos");
             System.out.println("5. Sumar calorías");
             System.out.println(Color.ROJO.getCode() + "0. Salir" + Color.RESET.getCode());
-            System.out.print("Seleccione una opción: ");
-            option = sc.nextInt();
-            sc.nextLine();
+            option = comprobarInt(sc, "Seleccione una opción: ");
 
             switch (option) {
                 case 1:
                     System.out.println("Ingrese la marca del detergente: ");
                     marca = sc.nextLine();
-                    System.out.println("Ingrese el precio del detergente: ");
-                    precio = sc.nextFloat();
-                    System.out.println("Ingrese el descuento del detergente: ");
-                    descuento = sc.nextFloat();
+
+                    precio = comprobarFloat(sc, "Ingrese el precio del detergente: ");
+                    
+                    descuento = comprobarFloat(sc, "Ingrese el descuento del detergente: ");
 
                     Detergente detergente = new Detergente(marca, precio, descuento);
                     detergentes.add(detergente);
+
                     System.out.println("Instanciado Detergente:\n " + detergente);
                     break;
 
                 case 2:
                     System.out.println("Ingrese la marca de los cereales: ");
                     marca = sc.nextLine();
-                    System.out.println("Ingrese el precio de los cereales: ");
-                    precio = sc.nextFloat();
-                    sc.nextLine();
+
+                    precio = comprobarFloat(sc, "Ingrese el precio de los cereales: ");
+
                     System.out.println("Ingrese el tipo de creal (espelta, maíz, trigo...): ");
                     tipoCereal = sc.nextLine();
 
                     Cereales cereal = new Cereales(marca, precio, tipoCereal);
                     cereales.add(cereal);
+
                     System.out.println("Instanciando Cereales:\n " + cereal);
                     break;
 
                 case 3:
                     System.out.println("Ingrese la marca del vino: ");
                     marca = sc.nextLine();
-                    System.out.println("Ingrese el precio del vino: ");
-                    precio = sc.nextFloat();
+
+                    precio = comprobarFloat(sc, "Ingrese el precio del vino: ");
+
                     System.out.println("Ingrese el tipo de vino (tinto, blanco, rosado...): ");
-                    sc.nextLine();
                     tipoVino = sc.nextLine();
-                    System.out.println("Ingrese el grado de alcohol del vino: ");
-                    gradoAlcohol = sc.nextInt();
+                    
+                    gradoAlcohol = comprobarFloat(sc, "Ingrese el grado de alcohol del vino: ");
 
                     Vino vino = new Vino(marca, tipoVino, gradoAlcohol, precio);
                     vinos.add(vino);
@@ -111,21 +111,30 @@ public class Main {
             System.out.println("2. Listar vinos");
             System.out.println(Color.ROJO.getCode() + "3. Para de añadir" + Color.RESET.getCode());
             System.out.print("Seleccione una opción: ");
-            opcion = sc.nextInt();
-            sc.nextLine();
+            opcion = comprobarInt(sc, "Seleccione una opción: ");
 
             switch (opcion) {
                 case 1:
-                    listarCereales();
+                    if (!cereales.isEmpty()) {
+                        listarCereales();
 
-                    caloriasTotales += sumarCalorias();
-                    break;
+                        caloriasTotales += sumarCalorias();
+                        break;
+                    } else {
+                        System.out.println(Color.AMARILLO.getCode() + "Aún no existen cereales." + Color.RESET.getCode());
+                        break;
+                    }
             
                 case 2:
-                    listarVinos();
+                    if (!vinos.isEmpty()) {
+                        listarVinos();
 
-                    caloriasTotales += sumarCalorias();
-                    break;
+                        caloriasTotales += sumarCalorias();
+                        break;
+                    } else {
+                        System.out.println(Color.AMARILLO.getCode() + "Aún no existen vinos." + Color.RESET.getCode());
+                        break;
+                    }
 
                 case 3:
                     System.out.println(Color.AMARILLO.getCode() + "Volviendo..." + Color.RESET.getCode());
@@ -178,5 +187,39 @@ public class Main {
         sc.nextLine();
 
         return cereales.get(seleccion).getCalorias();
+    }
+
+    public static float comprobarFloat(Scanner sc, String mensaje) {
+        float dato;
+
+        do {
+            try {
+                System.out.println(mensaje);
+                dato = sc.nextFloat();
+                sc.nextLine();
+            } catch (Exception e) {
+                System.out.println(Color.AMARILLO.getCode() + "Debe ingresar un número" + Color.RESET.getCode());
+                sc.nextLine();
+                dato = -1;
+            }
+        } while (dato < 0);
+        return dato;
+    }
+
+    public static int comprobarInt(Scanner sc, String mensaje) {
+        int dato;
+
+        do {
+            try {
+                System.out.println(mensaje);
+                dato = sc.nextInt();
+                sc.nextLine();
+            } catch (Exception e) {
+                System.out.println(Color.AMARILLO.getCode() + "Debe ingresar un número entero" + Color.RESET.getCode());
+                sc.nextLine();
+                dato = -1;
+            }
+        } while (dato < 0);
+        return dato;
     }
 }
